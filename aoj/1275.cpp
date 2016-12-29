e99fd954d307d2f3b6887947e577d3a21ba57f9a
nosi-llll <s1410068@edu.cc.uec.ac.jp>
2016-12-29 20:58:51 +0900

    ヨセフスの問題 初見×

--- aoj/1275.cpp
+++ aoj/1275.cpp
@@ -1,42 +1,41 @@
+/*
+	ヨセフスの問題
+	参考:
+	https://ja.wikipedia.org/wiki/%E3%83%A8%E3%82%BB%E3%83%95%E3%82%B9%E3%81%AE%E5%95%8F%E9%A1%8C
+	- wikipedia
+	http://imulan.hatenablog.jp/entry/2015/06/01/175701
+	- 裏紙
+	漸化式は
+	f(n,k) = (f(n-1,k) + k) % n
+	f(1,k) = 0
+	この問題がヨセフスの問題と違う点は
+	・始点が0（最初の人）ではなくmになる
+	・始点を取り除く
+	この二点を踏まえて考えると、最初に呼び出す際のf(n,k)を
+	f(n,k) = (f(n-1,k) + m) % n
+	に変形すると上手く行く。
+	解釈としては、
+	・ヨセフスの問題より最初から一人人数が少ないと考えて、f(n-1,k)とする
+	・始まりがm番目の人なので、f(n-1,k) + m
+
+	注意としては出力が1から数えているので、 + 1 を忘れないこと
+ */
 #include <iostream>
-#include <vector>
 #define REP(i,n) for (int i=0;i<(n);i++)
 
 using namespace std;
+
+int josephus(int n,int k){
+	if(n == 1)return 0;
+	else return (josephus(n-1,k) + k) % n;
+}
 
 int main(){
   cin.tie(0);
   ios::sync_with_stdio(false);
 	int n,k,m;
 	while(cin >> n >> k >> m,n){
-		vector<bool> stone(n,true);
-		int stone_ct = 0;
-		int index = m - 1;
-		int interval = 0;
-		while(true){
-			if(!stone[index]){
-				index = index + 1 < n ? index + 1 : 0;
-			}else if(interval == 0){
-				stone[index] = false;
-				stone_ct++;
-				//							cout << index << endl;
-				if(stone_ct == n - 1){
-					break;
-				}else{
-					interval = (k % (n - stone_ct) - 1 + n - stone_ct)%(n-stone_ct);
-					index = index + 1 < n ? index + 1 : 0;
-				}
-			}else{
-				index = index + 1 < n ? index + 1 : 0;
-				interval--;
-			}
-		}
-		REP(i,n){
-			if(stone[i]){
-						cout << i + 1 << endl;
-						break;
-			}
-		}
+		cout <<  (josephus(n - 1,k) + m) % n + 1<< endl;
 	}
   return 0;
 }
