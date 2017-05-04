18ae8b72523995374bdde350834a4af33031a220
nosi-llll <s1410068@edu.cc.uec.ac.jp>
2017-05-04 22:50:45 +0900

    Naive String Search

---
+++ aoj/alds/alds1_14a.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <bitset>
+#include <climits>
+#define REP(i,n) for (int i=0;i<(n);i++)
+#define FOR(i,a,b) for (int i=(a);i<(b);i++)
+#define RREP(i,n) for (int i=(n)-1;i>=0;i--)
+#define RFOR(i,a,b) for (int i=(a)-1;i>=(b);i--)
+#define ll long long
+#define ull unsigned long long
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
+const int INF = 1e9;
+const int MOD = 1e9 + 7;
+
+using namespace std;
+
+int main(){
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+	string T,P;
+	cin>>T>>P;
+	if(T.size() < P.size())return 0;
+	REP(i,T.size()){
+		bool found = true;
+		REP(j,P.size()){
+			if(i + j >= T.size() || T[i+j] != P[j]){
+				found = false;break;
+			}
+		}
+		if(found)cout << i << endl;
+	}
+  return 0;
+}
